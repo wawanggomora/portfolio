2d4f1f303a70f701a48bc13fd44fbc552d66f273
Joshua Gomora <joshuag@platypuslocal.com>
2020-05-02 14:06:32 +0800

    Updates

---
+++ precache-manifest.fb08996b46dca65d14955b6afaaa98f5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf497e2025d2a74bf4c0c1e7d3459e1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9412d2005bc699875476",
+    "url": "/portfolio/static/css/main.15f6fe6f.chunk.css"
+  },
+  {
+    "revision": "18348f12bfa00637ae56",
+    "url": "/portfolio/static/js/2.590b61f5.chunk.js"
+  },
+  {
+    "revision": "9412d2005bc699875476",
+    "url": "/portfolio/static/js/main.22d296f7.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "8ea511d5f2a006458a90e1f4cd37cdad",
+    "url": "/portfolio/static/media/bg-dots.8ea511d5.svg"
+  },
+  {
+    "revision": "3e60fec0ea9c583198cd361586b747a1",
+    "url": "/portfolio/static/media/bighead-thumb.3e60fec0.jpg"
+  },
+  {
+    "revision": "0dc793fb061b52ad3072286022e574d8",
+    "url": "/portfolio/static/media/confection-1.0dc793fb.jpg"
+  },
+  {
+    "revision": "0abcbf732c1b7a3ecc075661958aaf2e",
+    "url": "/portfolio/static/media/confection-2.0abcbf73.jpg"
+  },
+  {
+    "revision": "9c0c1ef0b10f496e1cb0de4032792a9b",
+    "url": "/portfolio/static/media/confection-thumb.9c0c1ef0.jpg"
+  },
+  {
+    "revision": "5a19d9be73b069b8d1e570b3255da853",
+    "url": "/portfolio/static/media/door-1.5a19d9be.jpg"
+  },
+  {
+    "revision": "986027d9cc26d2bcdc1d06e969a01171",
+    "url": "/portfolio/static/media/door-2.986027d9.jpg"
+  },
+  {
+    "revision": "315a2bb199a5824bfe6faaee3397f538",
+    "url": "/portfolio/static/media/dot-fade.315a2bb1.svg"
+  },
+  {
+    "revision": "22415ade04f1b6333f4dd046a06b64b4",
+    "url": "/portfolio/static/media/eden-thumb.22415ade.jpg"
+  },
+  {
+    "revision": "38b7cc7057cf2d942a8e0ada6e7bc2fc",
+    "url": "/portfolio/static/media/element-1.38b7cc70.svg"
+  },
+  {
+    "revision": "209f57b48573a39be90353dced705f96",
+    "url": "/portfolio/static/media/element-3.209f57b4.svg"
+  },
+  {
+    "revision": "1360b9437a1a4004e41f7504f22c0bd1",
+    "url": "/portfolio/static/media/element-4.1360b943.svg"
+  },
+  {
+    "revision": "2596115ced86c182ea74e05833a42b42",
+    "url": "/portfolio/static/media/furniture-1.2596115c.jpg"
+  },
+  {
+    "revision": "45394fc217a51bb96499eb694260f839",
+    "url": "/portfolio/static/media/furniture-2.45394fc2.jpg"
+  },
+  {
+    "revision": "9468bb036ebec136d88e7d3ed0b1e954",
+    "url": "/portfolio/static/media/josh-gomora.9468bb03.jpg"
+  },
+  {
+    "revision": "819985cd1190e98edceb97fc371c785d",
+    "url": "/portfolio/static/media/photo-bg.819985cd.png"
+  },
+  {
+    "revision": "764df8abd1e34fd7322df007a707abd5",
+    "url": "/portfolio/static/media/tattoo-1.764df8ab.jpg"
+  },
+  {
+    "revision": "0e16a84c13ec8fef6ca4e68be2b6d764",
+    "url": "/portfolio/static/media/tattoo-2.0e16a84c.jpg"
+  },
+  {
+    "revision": "ef8f19fd48abfcdcf8188131bbc21e81",
+    "url": "/portfolio/static/media/tattoo-thumb.ef8f19fd.jpg"
+  },
+  {
+    "revision": "e4a5baf2cf8674fdb79f9d476ca17c5a",
+    "url": "/portfolio/static/media/wave.e4a5baf2.svg"
+  }
+]);
